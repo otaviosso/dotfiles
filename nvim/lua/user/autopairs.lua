ab6c1b5be2cd81ca961a916176d7833e185ba824
otaviosso <otavioscarparosouza@gmail.com>
2023-01-18 19:29:18 -0300

    Autopairs

--- nvim/lua/user/autopairs.lua
+++ nvim/lua/user/autopairs.lua
@@ -1,10 +1,5 @@
 require "nvim-autopairs".setup{
 	check_ts = true,
-	ts_config = {
-		lua = { "string", "source" },
-    javascript = { "string", "template_string" },
-    java = false,
-  },
   disable_filetype = { "TelescopePrompt", "spectre_panel" },
   fast_wrap = {
     map = "<M-e>",
